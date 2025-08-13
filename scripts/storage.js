f23384fc6e5fd8ab83a806549e95c01cef5ae369
Sbabalo Nkompela <sbabalo.nkompela111@gmail.com>
2025-08-13 12:15:07 +0200

    updated loading task funtion

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,19 +1,29 @@
 // storage.js
 export function saveTasksToLocalStorage(tasks) {
   try {
+
     localStorage.setItem("kanbanTasks", JSON.stringify(tasks));
+
   } catch (error) {
+
     console.error("Error saving tasks to Loacal Storage:", error)
+
   }
 }
 
 export function loadTasksFromLocalStorage() {
-  const storedTasks = localStorage.getItem("kanbanTasks");
   try {
+
+    const storedTasks = localStorage.getItem("kanbanTasks");
+
     return storedTasks ? JSON.parse(storedTasks) : [];
+
   } catch (error) {
+
     console.error("Error loading tasks from local Storage:", error);
+
     return [];
+    
   }
 }
 
